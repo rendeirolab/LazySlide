48e65d8b079d88074b3cbcf757356187df3e3fe6
ernestoab <ernesto.abila@outlook.com>
2023-11-27 16:16:45 +0100

    add openslide script

--- lazyslide/readers/openslide.py
+++ lazyslide/readers/openslide.py
@@ -1,8 +1,8 @@
-import numpy as np
 import openslide
+from .base import ReaderBase
 
 
-class OpenSlideBackend(SlideBackend):
+class OpenSlideBackend(ReaderBase):
     """
     Use OpenSlide to interface with image files.
 
