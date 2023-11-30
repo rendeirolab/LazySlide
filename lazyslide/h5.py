8dd6e965d9a97b6929210424189b9b08ba253b16
Mr-Milk <yzheng@cemm.at>
2023-11-30 11:38:40 +0100

    Only acquire H5 file handler when needed

--- lazyslide/h5.py
+++ lazyslide/h5.py
@@ -15,89 +15,87 @@ class H5File:
 
     def __init__(self, h5_file):
         h5_file = Path(h5_file)
-        mode = "r+" if h5_file.exists() else "w"
-        self.file = h5py.File(h5_file, mode)
-        self.coords = self.load_coords()
-        self.tile_ops = self.load_tile_ops()
-        self.masks = self.load_masks()
+        # Create new file if not exist
+        if not h5_file.exists():
+            with h5py.File(h5_file, "w"):
+                pass
+        self.file = h5_file
+        self.coords = None
+        self.tile_ops = None
+        self.masks = None
+        self.load()
+
         self._rewrite = False
         self._rewrite_mask = True
-
-    def __exit__(self, *args):
-        """Close h5 handler when exit"""
-        self.file.close()
 
     def set_coords(self, coords):
         # Delete the previous exist one
         self.coords = coords
         self._rewrite = True
 
-    def load_coords(self):
-        if self.COORDS_KEY in self.file:
-            return self.file[self.COORDS_KEY][:]
+    def load(self):
+        with h5py.File(self.file, "r+") as h5:
+            if self.COORDS_KEY in h5:
+                self.coords = h5[self.COORDS_KEY][:]
+                h5_attrs = h5[self.COORDS_KEY].attrs
+                attrs = {}
+                for key in h5_attrs.keys():
+                    value = h5_attrs.get(key)
+                    if key == "mask_name" and value == 0:
+                        value = None
+                    attrs[key] = value
+                self.tile_ops = TileOps(**attrs)
+
+            if self.MASKS_KEY in h5:
+                masks = {}
+                masks_group = h5[self.MASKS_KEY]
+                for mask_name in masks_group.keys():
+                    masks[mask_name] = masks_group.get(mask_name)[:]
+                self.masks = masks
+            self.masks = {}
 
     def get_coords(self):
-        if self.coords is None:
-            self.coords = self.load_coords()
         return self.coords
 
     def set_tile_ops(self, tile_ops: TileOps):
         self.tile_ops = tile_ops
         self._rewrite = True
 
-    def load_tile_ops(self):
-        if self.COORDS_KEY in self.file:
-            h5_attrs = self.file[self.COORDS_KEY].attrs
-            attrs = {}
-            for key in h5_attrs.keys():
-                attrs[key] = h5_attrs.get(key)
-            return TileOps(**attrs)
-
     def get_tile_ops(self):
-        if self.tile_ops is None:
-            self.tile_ops = self.load_tile_ops()
         return self.tile_ops
 
     def set_mask(self, name, mask):
         self.masks[name] = mask
         self._rewrite_mask = True
 
-    def load_masks(self):
-        if self.MASKS_KEY in self.file:
-            masks = {}
-            masks_group = self.file[self.MASKS_KEY]
-            for mask_name in masks_group.keys():
-                masks[mask_name] = masks_group.get(mask_name)[:]
-            return masks
-        return {}
-
     def get_masks(self):
-        if self.masks is None:
-            self.masks = self.load_masks()
         return self.masks
 
     def save(self):
-        if self._rewrite:
-            # Delete the previous exist coords
-            if self.COORDS_KEY in self.file:
-                del self.file[self.COORDS_KEY]
+        with h5py.File(self.file, "r+") as h5:
+            if self._rewrite:
+                # Delete the previous exist coords
+                if self.COORDS_KEY in h5:
+                    del h5[self.COORDS_KEY]
 
-            shape = self.coords.shape
-            ds = self.file.create_dataset(self.COORDS_KEY, shape,
-                                          dtype=np.uint16,
-                                          chunks=True)
-            ds[:] = self.coords
-            attrs = ds.attrs
-            for k, v in asdict(self.tile_ops).items():
-                attrs[k] = v
+                shape = self.coords.shape
+                ds = h5.create_dataset(self.COORDS_KEY, shape,
+                                       dtype=np.uint16,
+                                       chunks=True)
+                ds[:] = self.coords
+                attrs = ds.attrs
+                for k, v in asdict(self.tile_ops).items():
+                    if k == "mask_name" and v is None:
+                        v = 0
+                    attrs[k] = v
 
-        if self._rewrite_mask:
-            # Delete the previous exist masks
-            if self.MASKS_KEY in self.file:
-                del self.file[self.MASKS_KEY]
+            if self._rewrite_mask:
+                # Delete the previous exist masks
+                if self.MASKS_KEY in h5:
+                    del h5[self.MASKS_KEY]
 
-            masks_group = self.file.create_group("masks")
-            for mask_name, mask_array in self.masks.items():
-                ds = masks_group.create_dataset(mask_name, shape=mask_array.shape,
-                                                dtype=np.uint16, chunks=True)
-                ds[:] = mask_array
+                masks_group = h5.create_group("masks")
+                for mask_name, mask_array in self.masks.items():
+                    ds = masks_group.create_dataset(mask_name, shape=mask_array.shape,
+                                                    dtype=np.uint16, chunks=True)
+                    ds[:] = mask_array
