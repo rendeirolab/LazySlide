34a345aeff0a4c5f0093f7cb9cd222a9aac24e14
Mr-Milk <yzheng@cemm.at>
2023-11-30 11:39:33 +0100

    Add an about function

--- lazyslide/__init__.py
+++ lazyslide/__init__.py
@@ -5,3 +5,10 @@ from .wsi import WSI, create_tiles_coords, filter_tiles
 from .readers.utils import get_crop_left_top_width_height
 from .cv_mods import TissueDetectionHE
 from .h5 import H5File
+from .utils import get_reader
+
+
+def about():
+    """Provide current information for the Lazyslide"""
+    print("Version:", __version__)
+    print("Backend:", get_reader().__name__)
