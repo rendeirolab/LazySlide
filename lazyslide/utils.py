5d6e07efda84e6d17d2a55e083af34083f716f0d
Mr-Milk <yzheng@cemm.at>
2024-01-25 16:46:29 +0100

    modify utils

--- lazyslide/utils.py
+++ lazyslide/utils.py
@@ -6,6 +6,9 @@ from itertools import tee
 from typing import Type
 from urllib.parse import urlparse
 
+import numpy as np
+from numba import njit
+
 import requests
 
 from .readers.base import ReaderBase
@@ -104,3 +107,204 @@ class TileOps:
     ops_height: int = field(default=None)
     ops_width: int = field(default=None)
     mask_name: str = field(default=None)
+
+
+@njit
+def _creat_tiles_params(image_shape, tile_w, tile_h, stride_w=None, stride_h=None, pad=True):
+    height, width = image_shape
+    if stride_w is None:
+        stride_w = tile_w
+    if stride_h is None:
+        stride_h = tile_h
+
+    # calculate number of expected tiles
+    if pad and width % stride_w != 0:
+        n_tiles_width = width // stride_w + 1
+    else:
+        n_tiles_width = (width - tile_w) // stride_w + 1
+    if pad and height % stride_h != 0:
+        n_tiles_height = height // stride_h + 1
+    else:
+        n_tiles_height = (height - tile_h) // stride_h + 1
+    return n_tiles_width, n_tiles_height
+
+
+@njit
+def create_tiles(
+        image_shape, tile_w, tile_h, stride_w=None, stride_h=None, pad=True
+):
+    """Create the tiles, return only coordination
+
+    Padding works as follows:
+    If ``pad is False``, then the first tile will start flush with the edge of the image, and the tile locations
+    will increment according to specified stride, stopping with the last tile that is fully contained in the image.
+    If ``pad is True``, then the first tile will start flush with the edge of the image, and the tile locations
+    will increment according to specified stride, stopping with the last tile which starts in the image. Regions
+    outside the image will be padded with 0.
+    For example, for a 5x5 image with a tile size of 3 and a stride of 2, tile generation with ``pad=False`` will
+    create 4 tiles total, compared to 6 tiles if ``pad=True``.
+
+    Parameters
+    ----------
+    image_shape : (int, int), The shape of the image
+    tile_w : int, The width of tile
+    tile_h : int, The height of tile
+    stride_w : int, The width of stride when move to next tile
+    stride_h : int, The height of stride when move to next tile
+    pad : bool, If ``True``, these edge tiles will be zero-padded
+                and yielded with the other chunks.
+                If ``False``, incomplete edge chunks will be ignored.
+                Defaults to ``False``.
+
+    Returns
+    -------
+
+    """
+    n_tiles_width, n_tiles_height = _creat_tiles_params(image_shape, tile_w, tile_h, stride_w, stride_h, pad)
+
+    coordinates = []
+    for ix_width in range(n_tiles_width):
+        for ix_height in range(n_tiles_height):
+            coords = (int(ix_width * stride_w), int(ix_height * stride_h))
+            coordinates.append(coords)
+
+    return np.array(coordinates, dtype=np.uint)
+
+
+@njit
+def create_tiles_coords_index(
+        image_shape, tile_w, tile_h, stride_w=None, stride_h=None, pad=True
+):
+    """Create the tiles, return coordination that comprise the tiles
+        and the index of points for each rect
+
+    Padding works as follows:
+    If ``pad is False``, then the first tile will start flush with the edge of the image, and the tile locations
+    will increment according to specified stride, stopping with the last tile that is fully contained in the image.
+    If ``pad is True``, then the first tile will start flush with the edge of the image, and the tile locations
+    will increment according to specified stride, stopping with the last tile which starts in the image. Regions
+    outside the image will be padded with 0.
+    For example, for a 5x5 image with a tile size of 3 and a stride of 2, tile generation with ``pad=False`` will
+    create 4 tiles total, compared to 6 tiles if ``pad=True``.
+
+    Parameters
+    ----------
+    image_shape : (int, int), The shape of the image
+    tile_h : int, The height of tile
+    tile_w : int, The width of tile
+    stride_h : int, The height of stride when move to next tile
+    stride_w : int, The width of stride when move to next tile
+    pad : bool, If ``True``, these edge tiles will be zero-padded
+                and yielded with the other chunks.
+                If ``False``, incomplete edge chunks will be ignored.
+                Defaults to ``False``.
+
+    Returns
+    -------
+
+    """
+    n_tiles_width, n_tiles_height = _creat_tiles_params(image_shape, tile_w, tile_h, stride_w, stride_h, pad)
+
+    coordinates = list()
+    indices = list()
+    for ix_width in range(n_tiles_width + 1):
+        for ix_height in range(n_tiles_height + 1):
+            coords = (int(ix_width * stride_w), int(ix_height * stride_h))
+            coordinates.append(coords)
+            if (ix_width != n_tiles_width) & (ix_height != n_tiles_height):
+                ix1 = ix_height * (n_tiles_width + 1) + ix_width
+                ix3 = (ix_height + 1) * (n_tiles_width + 1) + ix_width
+                indices.append([ix1, ix1 + 1, ix3 + 1, ix3])
+
+    return np.array(coordinates, dtype=np.uint), np.array(indices, dtype=np.uint)
+
+
+@njit
+def filter_tiles(mask, tiles_coords, tile_w, tile_h, filter_bg=0.8):
+    """Return a binary array that indicate which tile should be left
+
+    Parameters
+    ----------
+    mask
+    tiles_coords
+    filter_bg
+    tile_w,
+    tile_h,
+    Returns
+    -------
+
+    """
+    use = []
+    for w, h in tiles_coords:
+        mask_region = mask[h: h + tile_h, w: w + tile_w]
+        bg_ratio = np.sum(mask_region == 0) / mask_region.size
+        use.append(bg_ratio < filter_bg)
+    return np.array(use, dtype=np.bool_)
+
+
+def get_split_image_indices(image_height, image_width, min_side=20000):
+    h, w = image_height, image_width
+    size = h * w
+    n = min_side
+    if (size > n ** 2) or (h > n) or (w > n):
+        split_h = h > 1.5 * n
+        split_w = w > 1.5 * n
+
+        if not split_h and not split_w:
+            return
+
+        n_chunk_h = int(np.ceil(h / n))
+        n_chunk_w = int(np.ceil(w / n))
+
+        # If split, return the split chunks
+        # Else, it would take the whole
+        ix_h = (
+            np.linspace(start=0, stop=h, num=n_chunk_h + 1, dtype=int)
+            if split_h
+            else [0, h]
+        )
+        ix_w = (
+            np.linspace(start=0, stop=w, num=n_chunk_w + 1, dtype=int)
+            if split_w
+            else [0, w]
+        )
+
+        slices = []
+        for h1, h2 in pairwise(ix_h):
+            row = []
+            for w1, w2 in pairwise(ix_w):
+                row.append((h1, h2, w1, w2))
+            slices.append(row)
+        return slices
+
+
+@njit
+def create_mesh_array(shape, coords, h, w, values=None):
+    """Create a mesh array from a given image and coordinates
+
+    Parameters
+    ----------
+    img : np.ndarray
+        The image
+    coords : np.ndarray
+        The coordinates
+    h : int
+        The height of the tiles
+    w : int
+        The width of the tiles
+    values : np.ndarray
+        The values to fill in the mesh array
+
+    Returns
+    -------
+    np.ndarray
+        The mesh array
+    """
+    mesh = np.full(shape, fill_value=np.nan)
+    if values is None:
+        for cw, ch in coords:
+            mesh[int(ch):int(ch + h), int(cw):int(cw + w)] = 1
+    else:
+        for (cw, ch), v in zip(coords, values):
+            mesh[int(ch):int(ch + h), int(cw):int(cw + w)] = v
+    return mesh
