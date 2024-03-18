bd600725dbc64b5e67c0fd2f8d76feb61c7660f5
Mr-Milk <yzheng@cemm.at>
2024-03-18 12:58:02 +0100

    Add resize parameter

--- lazyslide/loader/loader.py
+++ lazyslide/loader/loader.py
@@ -7,6 +7,6 @@ with try_import() as _import:
 
 
 class FeatureExtractionLoader(DataLoader):
-    def __init__(self, wsi, **kwargs):
-        dataset = FeatureExtractionDataset(wsi)
+    def __init__(self, wsi, resize=None, **kwargs):
+        dataset = FeatureExtractionDataset(wsi, resize=resize)
         super().__init__(dataset, **kwargs)
