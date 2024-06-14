3df7fe0faeb31f1ff2d82325dde1fb4573f92f95
Mr-Milk <yzheng@cemm.at>
2024-06-14 17:08:46 +0200

    Update backend selection logic

--- lazyslide/reader/utils.py
+++ lazyslide/reader/utils.py
@@ -1,8 +1,10 @@
 from .openslide import OpenSlideReader
+from .cucim import CuCIMReader
+from .tiffslide import TiffSlideReader
 
 
 def get_reader(reader: str):
-    readers = {"openslide": None}
+    readers = {"openslide": None, "cucim": None, "tiffslide": None}
 
     try:
         import openslide
@@ -11,7 +13,21 @@ def get_reader(reader: str):
     except (ModuleNotFoundError, OSError) as _:
         pass
 
-    reader_candidates = ["openslide"]
+    try:
+        import tiffslide
+
+        readers["tiffslide"] = TiffSlideReader
+    except (ModuleNotFoundError, OSError) as _:
+        pass
+
+    try:
+        import cucim
+
+        readers["cucim"] = CuCIMReader
+    except (ModuleNotFoundError, OSError) as _:
+        pass
+
+    reader_candidates = ["openslide", "tiffslide", "cucim"]
     if reader == "auto":
         for i in reader_candidates:
             reader = readers.get(i)
