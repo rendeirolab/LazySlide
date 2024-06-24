11b717af88dd6250f6889041d1cf8d74d7947394
Mr-Milk <yzheng@cemm.at>
2024-06-24 15:22:42 +0200

    Add a utility function to get torch device

---
+++ lazyslide/utils.py
@@ -0,0 +1,11 @@
+def get_torch_device():
+    """Automatically get the torch device"""
+    import torch
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    return device
