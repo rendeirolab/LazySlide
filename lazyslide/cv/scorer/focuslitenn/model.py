3a715822a4f3fd4fabda4ef819764caf9bb56527
Mr-Milk <yzheng@cemm.at>
2024-07-09 13:13:10 +0200

    FocusLiteNN will now load with a fake object when torch is not available

--- lazyslide/cv/scorer/focuslitenn/model.py
+++ lazyslide/cv/scorer/focuslitenn/model.py
@@ -5,45 +5,49 @@ import numpy as np
 
 from lazyslide.cv.scorer.base import ScorerBase
 
-import lazy_loader
+try:
+    import torch
 
-torch = lazy_loader.load("torch")
+    class FocusLiteNN(torch.nn.Module):
+        """
+        A FocusLiteNN model for filtering out-of-focus regions in whole slide images.
+        """
 
+        def __init__(self, num_channel=2):
+            super().__init__()
+            self.num_channel = num_channel
+            self.conv = torch.nn.Conv2d(
+                3, self.num_channel, 7, stride=5, padding=1
+            )  # 47x47
+            self.maxpool = torch.nn.MaxPool2d(kernel_size=47)
+            if self.num_channel > 1:
+                self.fc = torch.nn.Conv2d(self.num_channel, 1, 1, stride=1, padding=0)
 
-class FocusLiteNN(torch.nn.Module):
-    """
-    A FocusLiteNN model for filtering out-of-focus regions in whole slide images.
-    """
+            for m in self.modules():
+                if isinstance(m, torch.nn.Conv2d):
+                    n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                    m.weight.data.normal_(0, math.sqrt(2.0 / n))
 
-    def __init__(self, num_channel=2):
-        super().__init__()
-        self.num_channel = num_channel
-        self.conv = torch.nn.Conv2d(
-            3, self.num_channel, 7, stride=5, padding=1
-        )  # 47x47
-        self.maxpool = torch.nn.MaxPool2d(kernel_size=47)
-        if self.num_channel > 1:
-            self.fc = torch.nn.Conv2d(self.num_channel, 1, 1, stride=1, padding=0)
+        def forward(self, x):
+            batch_size = x.size()[0]
 
-        for m in self.modules():
-            if isinstance(m, torch.nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            x = self.conv(x)
+            x = -self.maxpool(-x)  # minpooling
+            if self.num_channel > 1:
+                x = self.fc(x)
+            x = x.view(batch_size, -1)
 
-    def forward(self, x):
-        batch_size = x.size()[0]
+            return x
+except ImportError:
 
-        x = self.conv(x)
-        x = -self.maxpool(-x)  # minpooling
-        if self.num_channel > 1:
-            x = self.fc(x)
-        x = x.view(batch_size, -1)
-
-        return x
+    class FocusLiteNN:
+        pass
 
 
 def load_focuslite_model(device="cpu"):
     model = FocusLiteNN()
+    if not hasattr(model, "forward"):
+        raise ModuleNotFoundError("To use Focuslite, you need to install pytorch")
     ckpt = torch.load(
         Path(__file__).parent / "focuslitenn-2kernel-mse.pt", map_location=device
     )
