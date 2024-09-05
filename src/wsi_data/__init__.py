2f49427ae7fb91b33bbb1944578c4277c244879c
Yimin Zheng <yzheng@cemm.at>
2024-09-05 10:27:03 +0200

    Clean up imports

--- src/wsi_data/__init__.py
+++ src/wsi_data/__init__.py
@@ -1,2 +1,2 @@
 from .data import WSIData, TileSpec
-from .io import open_wsi, agg_wsi, Downloader
+from .io import open_wsi, agg_wsi
--- src/wsi_data/io/__init__.py
+++ src/wsi_data/io/__init__.py
@@ -1,2 +1,1 @@
 from ._open_wsi import open_wsi, agg_wsi
-from ._download import Downloader
