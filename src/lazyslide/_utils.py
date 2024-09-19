60dc5f3c5b0a4ef2b0ba378834dc4d21ec2d244f
Yimin Zheng <yzheng@cemm.at>
2024-09-19 12:01:37 +0200

    Include the find_stack_level helper function

--- src/lazyslide/_utils.py
+++ src/lazyslide/_utils.py
@@ -1,3 +1,7 @@
+import inspect
+import os
+from types import FrameType
+
 from rich.console import Console
 
 console = Console()
@@ -46,3 +50,32 @@ def chunker(seq, num_workers):
         last += avg
 
     return out
+
+
+def find_stack_level() -> int:
+    """
+    Find the first place in the stack that is not inside pandas
+    (tests notwithstanding).
+    """
+
+    import pandas as pd
+
+    pkg_dir = os.path.dirname(pd.__file__)
+    test_dir = os.path.join(pkg_dir, "tests")
+
+    # https://stackoverflow.com/questions/17407119/python-inspect-stack-is-slow
+    frame: FrameType | None = inspect.currentframe()
+    try:
+        n = 0
+        while frame:
+            filename = inspect.getfile(frame)
+            if filename.startswith(pkg_dir) and not filename.startswith(test_dir):
+                frame = frame.f_back
+                n += 1
+            else:
+                break
+    finally:
+        # See note in
+        # https://docs.python.org/3/library/inspect.html#inspect.Traceback
+        del frame
+    return n
