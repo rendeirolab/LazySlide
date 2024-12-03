5061eaf40f1195ba66e7298d84f4811b83a7027d
Yimin Zheng <yzheng@cemm.at>
2024-12-03 11:07:29 +0100

    Bump version

--- src/lazyslide/__init__.py
+++ src/lazyslide/__init__.py
@@ -1,10 +1,7 @@
 """Efficient and Scalable Whole Slide Image (WSI) processing library."""
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from . import pp
 from . import tl
 from . import pl
 from . import models as models
-
-# Re-export
-from wsidata import open_wsi, agg_wsi
