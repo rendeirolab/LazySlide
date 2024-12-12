a834d0e060c376953d5431b26f690852451ba420
Yimin Zheng <yzheng@cemm.at>
2024-12-12 13:03:44 +0100

    API changes and version bump

--- src/lazyslide/__init__.py
+++ src/lazyslide/__init__.py
@@ -1,7 +1,10 @@
 """Efficient and Scalable Whole Slide Image (WSI) processing library."""
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from . import pp
 from . import tl
 from . import pl
 from . import models as models
+
+# Re-export the public API
+from wsidata import open_wsi, agg_wsi
--- src/lazyslide/_plotting/__init__.py
+++ src/lazyslide/_plotting/__init__.py
@@ -1,2 +1,3 @@
 from ._api import tissue, tiles
 from ._report import qc_summary
+from ._wsi_viewer import WSIViewer
