d6c248c686926bc581e698774feadd52535bd40b
Yimin Zheng <yzheng@cemm.at>
2025-04-27 10:42:43 +0200

    Skip testing timm models on CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import torch
 
@@ -34,3 +36,11 @@ def torch_jit_file(tmp_path_session):
     model = MockNet()
     torch.jit.script(model).save(tmp_path_session / "jit_model.pt")
     return tmp_path_session / "jit_model.pt"
+
+
+def pytest_collection_modifyitems(config, items):
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        skip_on_ci = pytest.mark.skip(reason="Skipped on GitHub CI")
+        for item in items:
+            if "skip_on_ci" in item.keywords:
+                item.add_marker(skip_on_ci)
--- tests/test_tl.py
+++ tests/test_tl.py
@@ -1,3 +1,4 @@
+import pytest
 import lazyslide as zs
 
 TIMM_MODEL = "mobilenetv3_small_050"
@@ -8,20 +9,12 @@ class TestFeatureExtraction:
         zs.pp.find_tissues(wsi)
         zs.pp.tile_tissues(wsi, 512)
         zs.tl.feature_extraction(wsi, model_path=torch_model_file)
+        # Test feature aggregation
+        zs.tl.feature_aggregation(wsi, feature_key=TIMM_MODEL)
 
     def test_load_jit_model(self, wsi, torch_jit_file):
         zs.tl.feature_extraction(wsi, model_path=torch_jit_file)
 
+    @pytest.mark.skip_on_ci
     def test_timm_model(self, wsi):
         zs.tl.feature_extraction(wsi, model=TIMM_MODEL)
-
-
-class TestFeatureAggregation:
-    def test_feature_aggregation(self, wsi):
-        # Prepare data
-        zs.pp.find_tissues(wsi)
-        zs.pp.tile_tissues(wsi, 512)
-        zs.tl.feature_extraction(wsi, model=TIMM_MODEL)
-
-        # Test feature aggregation
-        zs.tl.feature_aggregation(wsi, feature_key=TIMM_MODEL)
