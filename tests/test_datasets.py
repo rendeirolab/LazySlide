47e9df9d188919122b7bdbc131c43c4874e975d9
Yimin Zheng <yzheng@cemm.at>
2025-04-27 10:27:20 +0200

    Try to fix segmentation fault on GitHub Actions macOS runners

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,4 +2,15 @@ import lazyslide as zs
 
 
 def test_load_sample():
-    zs.datasets.sample()
+    wsi = zs.datasets.sample()
+    assert wsi is not None
+
+
+def test_load_gtex_artery():
+    wsi = zs.datasets.gtex_artery()
+    assert wsi is not None
+
+
+def test_load_lung_carcinoma():
+    wsi = zs.datasets.lung_carcinoma()
+    assert wsi is not None
--- tests/test_tl.py
+++ tests/test_tl.py
@@ -1,5 +1,7 @@
 import lazyslide as zs
 
+TIMM_MODEL = "mobilenetv3_small_050"
+
 
 class TestFeatureExtraction:
     def test_load_model(self, wsi, torch_model_file):
@@ -10,5 +12,16 @@ class TestFeatureExtraction:
     def test_load_jit_model(self, wsi, torch_jit_file):
         zs.tl.feature_extraction(wsi, model_path=torch_jit_file)
 
-    def test_torch_hub_model(self, wsi):
-        zs.tl.feature_extraction(wsi, model="resnet18")
+    def test_timm_model(self, wsi):
+        zs.tl.feature_extraction(wsi, model=TIMM_MODEL)
+
+
+class TestFeatureAggregation:
+    def test_feature_aggregation(self, wsi):
+        # Prepare data
+        zs.pp.find_tissues(wsi)
+        zs.pp.tile_tissues(wsi, 512)
+        zs.tl.feature_extraction(wsi, model=TIMM_MODEL)
+
+        # Test feature aggregation
+        zs.tl.feature_aggregation(wsi, feature_key=TIMM_MODEL)
