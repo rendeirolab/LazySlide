f718ce71377218201bc64cb9e98bb9ef77143e6b
ernestoab <ernesto.abila@outlook.com>
2025-05-30 16:48:28 +0200

    add hibou and midnight models

--- src/lazyslide/models/vision/__init__.py
+++ src/lazyslide/models/vision/__init__.py
@@ -5,3 +5,5 @@ from .uni import UNI, UNI2
 from .virchow import Virchow, Virchow2
 from .phikon import Phikon, PhikonV2
 from .h_optimus import HOptimus0, HOptimus1, H0Mini
+from .midnight import Midnight
+from .hibou import HibouB, HibouL
