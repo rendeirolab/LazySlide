f461ade8cf1aca6c19c442def986da1d1111ab3c
Yimin Zheng <yzheng@cemm.at>
2025-06-02 22:03:54 +0200

    Reimplement polygon merging algorithms

--- src/lazyslide/cv/tiles_merger.py
+++ src/lazyslide/cv/tiles_merger.py
@@ -4,6 +4,7 @@ from itertools import combinations
 
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 from shapely.ops import unary_union
 from shapely.strtree import STRtree
 
@@ -26,8 +27,6 @@ class PolygonMerger:
         The column that specify the probabilities of the polygons.
     buffer_px : float, default: 0
         The buffer size for the polygons to test the intersection.
-    drop_overlap : float, default: 0.9
-        The ratio to drop the overlapping polygons.
 
     """
 
@@ -37,13 +36,11 @@ class PolygonMerger:
         class_col: str = None,
         prob_col: str = None,
         buffer_px: float = 0,
-        drop_overlap: float = 0.9,
     ):
         self.gdf = gdf
         self.class_col = class_col
         self.prob_col = prob_col
         self.buffer_px = buffer_px
-        self.drop_overlap = drop_overlap
 
         self._has_class = class_col in gdf.columns if class_col else False
         self._has_prob = prob_col in gdf.columns if prob_col else False
@@ -53,8 +50,7 @@ class PolygonMerger:
     def _preprocess_polys(self):
         """Preprocess the polygons."""
         new_gdf = self.gdf.copy()
-        if self.buffer_px > 0:
-            new_gdf["geometry"] = self.gdf["geometry"].buffer(self.buffer_px)
+        new_gdf["geometry"] = self.gdf["geometry"].buffer(self.buffer_px)
         # Filter out invalid and empty geometries efficiently
         return new_gdf[new_gdf["geometry"].is_valid & ~new_gdf["geometry"].is_empty]
 
@@ -99,78 +95,95 @@ class PolygonMerger:
             groups_ix = np.sort(list(groups_ix))
 
             # Merge the group
-            merged_geoms = []  # (polygon, row_ix, groups_ix)
-
             if len(groups_ix) == 1:
                 ix = groups_ix[0]
-                m_geoms = polygons[ix]
-                merged_geoms.append((m_geoms, ix, groups_ix))
-            else:
-                m_geoms = [polygons[g] for g in groups_ix]
-                if self._has_class:
-                    ref_df = gpd.GeoDataFrame(
-                        {
-                            "names": [gdf[self.class_col].values[g] for g in groups_ix],
-                            "index": groups_ix,
-                            "geometry": m_geoms,
-                        }
-                    )
-
-                    # {class_name: polygon}
-                    named_polys = (
-                        ref_df[["names", "geometry"]]
-                        .groupby("names")
-                        .apply(unary_union)
-                        .to_dict()
-                    )
-
-                    if self.drop_overlap > 0:
-                        # If the two classes instances are more than 90% overlapping
-                        # The smaller one is removed
-                        while len(named_polys) > 1:
-                            names = list(named_polys.keys())
-                            combs = combinations(names, 2)
-                            for n1, n2 in combs:
-                                if n1 in named_polys and n2 in named_polys:
-                                    p1, p2 = named_polys[n1], named_polys[n2]
-                                    if p1.intersection(p2).is_empty:
-                                        continue
-                                    area, drop = (
-                                        (p1.area, n1)
-                                        if p1.area < p2.area
-                                        else (p2.area, n2)
-                                    )
-                                    union = p1.union(p2).area
-                                    overlap_ratio = union / area
-                                    if overlap_ratio > self.drop_overlap:
-                                        del named_polys[drop]
-                            break
-                    for n, p in named_polys.items():
-                        gs = ref_df[ref_df["names"] == n]["index"].tolist()
-                        merged_geoms.append((p, gs[0], gs))
-                else:
-                    m_geoms = unary_union(m_geoms)
-                    merged_geoms.append((m_geoms, groups_ix[0], groups_ix))
-            # Postprocess the merged polygon
-            for m_geom, ix, gs_ix in merged_geoms:
+                m_geom = polygons[ix]
                 if self.buffer_px > 0:
                     m_geom = m_geom.buffer(-self.buffer_px).buffer(0)
+                else:
+                    m_geom = m_geom.buffer(0)
+                record = {"geometry": m_geom}
+                if self._has_prob:
+                    prob = gdf.iloc[ix][self.prob_col]
+                    record[self.prob_col] = prob
+                merged.append(record)
+            else:
+                m_geoms = [polygons[g] for g in groups_ix]
+                # if self._has_class:
+                #     ref_df = gpd.GeoDataFrame(
+                #         {
+                #             "names": [gdf[self.class_col].values[g] for g in groups_ix],
+                #             "index": groups_ix,
+                #             "geometry": m_geoms,
+                #         }
+                #     )
+                #
+                #     # {class_name: polygon}
+                #     named_polys = (
+                #         ref_df[["names", "geometry"]]
+                #         .groupby("names")
+                #         .apply(unary_union)
+                #         .to_dict()
+                #     )
+                #
+                #     if self.drop_overlap > 0:
+                #         # If the two classes instances are more than 90% overlapping
+                #         # The smaller one is removed
+                #         while len(named_polys) > 1:
+                #             names = list(named_polys.keys())
+                #             combs = combinations(names, 2)
+                #             for n1, n2 in combs:
+                #                 if n1 in named_polys and n2 in named_polys:
+                #                     p1, p2 = named_polys[n1], named_polys[n2]
+                #                     if p1.intersection(p2).is_empty:
+                #                         continue
+                #                     area, drop = (
+                #                         (p1.area, n1)
+                #                         if p1.area < p2.area
+                #                         else (p2.area, n2)
+                #                     )
+                #                     union = p1.union(p2).area
+                #                     overlap_ratio = union / area
+                #                     if overlap_ratio > self.drop_overlap:
+                #                         del named_polys[drop]
+                #             break
+                #     for n, p in named_polys.items():
+                #         gs = ref_df[ref_df["names"] == n]["index"].tolist()
+                #         merged_geoms.append((p, gs[0], gs))
+                # else:
+                m_geom = unary_union(m_geoms)
+                if self.buffer_px > 0:
+                    m_geom = m_geom.buffer(-self.buffer_px).buffer(0)
+                else:
+                    m_geom = m_geom.buffer(0)
+
                 if m_geom.is_valid & (m_geom.is_empty is False):
-                    m_data = gdf.iloc[ix].copy()
-                    m_data["geometry"] = m_geom
+                    record = {"geometry": m_geom}
                     if self._has_prob:
-                        gs_gdf = gdf.iloc[gs_ix]
-                        m_data[self.prob_col] = np.average(
+                        gs_gdf = gdf.iloc[groups_ix]
+                        prob = np.average(
                             gs_gdf[self.prob_col], weights=gs_gdf["geometry"].area
                         )
-                    merged.append(m_data)
-            for g in groups_ix:
-                visited.add(g)
+                        record[self.prob_col] = prob
+                    merged.append(record)
+            visited.update(groups_ix)
         return gpd.GeoDataFrame(merged)
 
     def merge(self):
         """Launch the merging process."""
-        self._merged_polygons = self._tree_merge(self._preprocessed_polygons)
+        results = []
+        if self._has_class:
+            for c, polys in self._preprocessed_polygons.groupby(self.class_col):
+                merged_polys = self._tree_merge(polys)
+                merged_polys[self.class_col] = c
+                results.append(merged_polys)
+        else:
+            merged_polys = self._tree_merge(self._preprocessed_polygons)
+            results.append(merged_polys)
+
+        self._merged_polygons = gpd.GeoDataFrame(
+            pd.concat(results, ignore_index=True)
+        ).reset_index(drop=True)
 
     @property
     def merged_polygons(self):
@@ -182,9 +195,8 @@ def merge_polygons(
     class_col: str = None,
     prob_col: str = None,
     buffer_px: float = 0,
-    drop_overlap: float = 0.9,
 ):
-    merger = PolygonMerger(gdf, class_col, prob_col, buffer_px, drop_overlap)
+    merger = PolygonMerger(gdf, class_col, prob_col, buffer_px)
     merger.merge()
     return merger.merged_polygons
 
