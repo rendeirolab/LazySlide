32145eab6ffea260aabb1b9d119abe8c32394bef
Yimin Zheng <yzheng@cemm.at>
2025-07-26 21:51:05 +0200

    Add `gtex_small_intestine` dataset and migrate dataset fetching to Hugging Face Hub

--- src/lazyslide/datasets/_sample.py
+++ src/lazyslide/datasets/_sample.py
@@ -1,32 +1,43 @@
-import pooch
+import warnings
+from pathlib import Path
+
+from huggingface_hub import hf_hub_download
 from wsidata import open_wsi
 
-ENTRY = pooch.create(
-    path=pooch.os_cache("lazyslide"),
-    base_url="https://lazyslide.blob.core.windows.net/lazyslide-data",
-    registry={
-        "sample.svs": "sha256:ed92d5a9f2e86df67640d6f92ce3e231419ce127131697fbbce42ad5e002c8a7",
-        "sample.zarr.zip": "sha256:075a3ab61e6958673d79612cc29796a92cf875ad049fc1fe5780587968635378",
-        "GTEX-1117F-0526.svs": "sha256:222ab7f2bb42dcd0bcfaccd910cb13be452b453499e6117ab553aa6cd60a135e",
-        "GTEX-1117F-0526.zarr.zip": "sha256:2323b656322d2dcc7e9d18aaf586b39a88bf8f2a3959f642f109eb54268f3732",
-        "lung_carcinoma.ndpi": "sha256:3297b0a564f22940208c61caaca56d97ba81c9b6b7816ebc4042a087e557f85e",
-        "lung_carcinoma.zarr.zip": "sha256:0a8ccfc608f55624b473c6711b55739c3279d3b6fc5b654395dfc23b010bf866",
-    },
-)
+from lazyslide._utils import find_stack_level
 
-logger = pooch.get_logger()
-logger.setLevel("WARNING")
+REGISTRY = [
+    "sample.svs",
+    "GTEX-1117F-0526.svs",
+    "lung_carcinoma.ndpi",
+]
 
 
 def _load_dataset(slide_file, zarr_file, with_data=True, pbar=False):
-    slide = ENTRY.fetch(slide_file)
-    _ = ENTRY.fetch(
-        zarr_file,
-        progressbar=pbar,
-        processor=pooch.Unzip(extract_dir=zarr_file.rstrip(".zip")),
+    if pbar:
+        warnings.warn(
+            "pbar is deprecated in datasets",
+            DeprecationWarning,
+            stacklevel=find_stack_level(),
+        )
+
+    slide = hf_hub_download(
+        "RendeiroLab/LazySlide-data", slide_file, repo_type="dataset"
     )
-    store = "auto" if with_data else None
-    return open_wsi(slide, store=store)
+    slide_zarr = None
+    if with_data:
+        slide_zarr_zip = hf_hub_download(
+            "RendeiroLab/LazySlide-data", zarr_file, repo_type="dataset"
+        )
+        slide_zarr = slide_zarr_zip.replace(".zip", "")
+        # Unzip the zarr file if it is a zip file
+        # But only if it is not already unzipped
+        if not Path(slide_zarr).exists():
+            from zipfile import ZipFile
+
+            with ZipFile(slide_zarr_zip, "r") as zip_ref:
+                zip_ref.extractall(slide_zarr)
+    return open_wsi(slide, store=slide_zarr if with_data else None, pbar=pbar)
 
 
 def sample(with_data: bool = True, pbar: bool = False):
@@ -65,6 +76,28 @@ def gtex_artery(with_data: bool = True, pbar: bool = False):
     return _load_dataset(
         "GTEX-1117F-0526.svs",
         "GTEX-1117F-0526.zarr.zip",
+        with_data=with_data,
+        pbar=pbar,
+    )
+
+
+def gtex_small_intestine(with_data: bool = True, pbar: bool = False):
+    """
+    A small GTEX artery slide for testing purposes.
+
+    Source: https://gtexportal.org/home/histologyPage, GTEX-11DXX-1626.svs
+
+    Parameters
+    ----------
+    with_data : bool, default: True
+        Whether to load the associated zarr storage data.
+    pbar : bool, default: False
+        Whether to show the progress bar.
+
+    """
+    return _load_dataset(
+        "GTEX-11DXX-1626.svs",
+        "GTEX-11DXX-1626.zarr.zip",
         with_data=with_data,
         pbar=pbar,
     )
