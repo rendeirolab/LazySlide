4f99e20f2b6723876c71d855ad6878d5c82a0f90
Yimin Zheng <yzheng@cemm.at>
2025-08-31 17:11:02 +0200

    Remove feature_utag API

--- src/lazyslide/tools/__init__.py
+++ src/lazyslide/tools/__init__.py
@@ -1,7 +1,7 @@
 from ._domain import spatial_domain, tile_shaper
 from ._features import feature_aggregation, feature_extraction
 from ._signatures import RNALinker
-from ._spatial_features import feature_utag, spatial_features
+from ._spatial_features import spatial_features
 from ._text_annotate import text_embedding, text_image_similarity
 from ._tile_prediction import tile_prediction
 from ._tissue_props import tissue_props
