7132f5412d8e5f70ea4f165db718a3a8e20c93af
Yimin Zheng <yzheng@cemm.at>
2026-04-26 16:03:32 +0200

    Update model test suites

--- packages/lazyslide-models/tests/test_general.py
+++ packages/lazyslide-models/tests/test_general.py
@@ -3,8 +3,7 @@ from typing import List
 import pytest
 import torch
 from huggingface_hub.errors import GatedRepoError
-
-from lazyslide.models import MODEL_REGISTRY, ModelTask
+from lazyslide_models import MODEL_REGISTRY, ModelTask
 
 MODELS = list(MODEL_REGISTRY.keys())
 
@@ -88,20 +87,15 @@ def test_model_init(model_name):
         model = MODEL_REGISTRY[model_name]()
     except GatedRepoError:
         pytest.skip(f"{model_name} is not available.")
-        return
     except ModuleNotFoundError:
         pytest.skip(f"{model_name} has dependencies that are not installed.")
-        return
     except NotImplementedError:
         pytest.skip(f"{model_name} maybe deprecated or not supported yet.")
-        return
 
     # Test all attributes
-    if model.task == ModelTask.cv_feature:
-        assert model.name is not None
-    else:
+    assert model.name is not None
+    if model.task != ModelTask.cv_feature:
         assert model.model is not None
-        assert model.name is not None
         assert isinstance(model.task, (ModelTask, List))
         assert model.license is not None
         assert model.commercial is not None
